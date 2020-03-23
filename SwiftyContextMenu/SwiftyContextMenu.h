3b0f407e69b2bbeddd7959655ac10d286dedc552
behzad94 <behzad.m9494@gmail.com>
2020-03-23 17:38:29 +0430

    Initial project setup

---
+++ SwiftyContextMenu/SwiftyContextMenu.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyContextMenu.h
+//  SwiftyContextMenu
+//
+//  Created by DIAKO on 3/22/20.
+//  Copyright © 2020 Diako. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyContextMenu.
+FOUNDATION_EXPORT double SwiftyContextMenuVersionNumber;
+
+//! Project version string for SwiftyContextMenu.
+FOUNDATION_EXPORT const unsigned char SwiftyContextMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyContextMenu/PublicHeader.h>
+
+
